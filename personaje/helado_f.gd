039cb38b72c6b7a112ce81b22141a083c4aee8aa
francoheredev <franco_mapa@outlook.com>
2026-04-22 17:48:20 -0300

    agregué juice de correr
    particulas al correr

--- personaje/helado_f.gd
+++ personaje/helado_f.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var death_particles_scene: PackedScene
+
 # --------------------
 # VELOCIDAD (SMB)
 # --------------------
@@ -54,10 +55,10 @@ var wall_jump_lock_timer := 0.0
 var dead := false
 
 @onready var anim = $AnimatedSprite2D
+@onready var run_particles = $RunParticles  # 🔥 NUEVO
 
 
 func _physics_process(delta):
-	# 🔥 si está muerto, no se mueve
 	if dead:
 		return
 
@@ -87,7 +88,7 @@ func _physics_process(delta):
 		jump_buffer_timer = max(jump_buffer_timer - delta, 0)
 
 	# --------------------
-	# WALL JUMP (SMB + momentum)
+	# WALL JUMP
 	# --------------------
 	if on_wall and not on_floor and Input.is_action_just_pressed("jump"):
 		velocity.x = (wall_jump_force.x * wall_dir) + (velocity.x * 0.3)
@@ -152,55 +153,77 @@ func _physics_process(delta):
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y *= jump_cut
 
+	# --------------------
+	# RUN PARTICLES 🔥
+	# --------------------
+	var is_moving = abs(velocity.x) > 80
+
+	if on_floor and is_moving:
+		run_particles.emitting = true
+		
+		# salen desde atrás del personaje
+		if velocity.x > 0:
+			run_particles.position.x = -6
+		else:
+			run_particles.position.x = 6
+	else:
+		run_particles.emitting = false
+
 	move_and_slide()
 	update_animations(input_dir)
 
 
 # --------------------
-# MUERTE (SMB)
+# MUERTE
 # --------------------
 func die():
 	if dead:
 		return
 	
-	dead = true  # 🔥 evita doble trigger SIEMPRE
+	dead = true
 
 	GameManager.deaths += 1
 
-	print("muerto")
-
-	# 🎬 animación
 	anim.play("death")
 
-	# 💥 partículas (seguro)
 	if death_particles_scene:
 		var particles = death_particles_scene.instantiate()
 		particles.global_position = global_position
 		particles.z_index = 100
 		particles.emitting = true
 		get_parent().add_child(particles)
-	else:
-		print("⚠️ No asignaste death_particles_scene")
 
-	# 🧊 freeze tipo SMB
 	Engine.time_scale = 0.1
 	await get_tree().create_timer(0.08).timeout
 	Engine.time_scale = 1
 
-	# ⏳ tiempo para que se vea todo
 	await get_tree().create_timer(0.2).timeout
 
 	get_tree().reload_current_scene()
+
+
+# --------------------
+# ANIMACIONES
+# --------------------
 func update_animations(input_dir):
+	var dir = Input.get_axis("move_left", "move_right")
 	var is_running = Input.is_action_pressed("run")
 
-	if abs(velocity.x) > 20:
-		if velocity.x > 0:
-			play_anim("der2" if is_running else "der")
-		else:
-			play_anim("izq2" if is_running else "izq")
-	else:
+	if dir == 0:
 		play_anim("idle")
+		return
+
+	if not is_running:
+		if dir > 0:
+			play_anim("der")
+		else:
+			play_anim("izq")
+		return
+
+	if dir > 0:
+		play_anim("der2")
+	else:
+		play_anim("izq2")
 
 
 func play_anim(name):
